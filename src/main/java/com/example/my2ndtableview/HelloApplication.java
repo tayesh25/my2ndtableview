a22b5ebfe4b38ecb5f7b68b8ec00d2b07e790ac7
liamhood1 <lhood24@scienceleadership.org>
2023-12-22 09:43:46 -0500

    pee

--- src/main/java/com/example/my2ndtableview/HelloApplication.java
+++ src/main/java/com/example/my2ndtableview/HelloApplication.java
@@ -1,3 +1,4 @@
+package com.example.my2ndtableview;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
