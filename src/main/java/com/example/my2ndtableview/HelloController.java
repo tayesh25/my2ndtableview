ce1b64e521970f0a2c066c0cecdc7bc16d6b4feb
tayesh25 <tayesh25@sciencleadership.org>
2023-12-22 10:04:17 -0500

    liam hood king of the hood

--- src/main/java/com/example/my2ndtableview/HelloController.java
+++ src/main/java/com/example/my2ndtableview/HelloController.java
@@ -2,13 +2,29 @@ package com.example.my2ndtableview;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    public TableColumn left;
+     String name;
 
+    public TableColumn middle;
+    String park;
+
+    public TableColumn right;
+    Integer speed;
+
+
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
+
 
 
   //Millennium Force
